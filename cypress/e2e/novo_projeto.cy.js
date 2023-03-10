657170040928f9a0ac5a7a07997d7abd8c73dd35
Allan Ferraz <allansilvaferraz@hotmail.com>
2023-03-10 14:36:31 -0300

    atuando no primeiro site para ser automatizado

--- cypress/e2e/novo_projeto.cy.js
+++ cypress/e2e/novo_projeto.cy.js
@@ -1,10 +1,32 @@
 /// <reference types ="cypress" />
 
 
-describe('acessando o google', () => {
-    it('acessar o google', () => {
-        cy.visit('www.google.com.br');
-    });
+describe('Acessar site para testes automatizados', () => {
 
+    beforeEach(()=>{
+        cy.visit('https://demo.automationtesting.in/Register.html')
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+          })
+    })
+
+    it('Preenchendo dados básicos', () => {
+        cy.get('input[ng-model="FirstName"]').type('Allan');
+        cy.get('input[ng-model="LastName"]').type('Ferraz');
+     //   cy.get('input[ng-model="Adress"]').type('Rua de teste do Allan');
+        cy.get('input[ng-model="EmailAdress"]').type('allansilvaferraz@hotmail.com');
+        cy.get('input[ng-model="Phone"]').type('(17)912345678');
+        cy.get('input[value="Male"]').click();
+        cy.get('input[id="checkbox2"]').click();
+     //   cy.get('#msdd').select(['Portuguese']); não é possivel selecionar o campo
+        cy.get('#Skills').select('Android');
+       // cy.get('.select2-selection').type('Australia');
+        cy.get('.select2-selection').type('Australia').type('{downArrow}').select('Australia');
+        
+        //cy.get('input[class="select2-results__option select2-results__option--highlighted"]').select('Australia')
+        //cy.get('#yearbox').select('1996');
+    });
 
 });
